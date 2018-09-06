f7ae08a7930538bc876643cb69cdc25bc4151834
Wayne Collier <ke4tri@gmail.com>
2018-09-06 10:58:28 -0500

    created object for all three languages

--- main.js
+++ main.js
@@ -9,3 +9,44 @@ let concat_word = (word1,word2) => {
     return word3
 };
 
+const words = {
+    spanish : [
+    {
+    "merry":"alegre",
+    "christmas":"navida",
+    "and":"y",
+    "happy":"contento",
+    "new":"nuevo",
+    "year":"ano",
+    "good":"bueno",
+    "lord":"dio",
+    "snow":"nieve",
+    }
+    ],
+    French : [
+    {
+    "merry":"joyeux",
+    "christmas":"noel",
+    "and":"et",
+    "happy":"content",
+    "new":"nouveau",
+    "year":"an",
+    "good":"bien",
+    "lord":"seigneur",
+    "snow":"neige",
+    }
+    ],
+    Italian : [
+    {
+    "merry":"buon",
+    "christmas":"natale",
+    "and":"e",
+    "happy":"buon",
+    "new":"nuovo",
+    "year":"ano",
+    "good":"bueno",
+    "lord":"signore",
+    "snow":"la neve",
+    }
+    ]        
+}
